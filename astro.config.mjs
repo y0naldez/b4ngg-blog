68cea6139f85ab74c21b86d12cde955dd24d3f61
y0naldez <yonaldez0@gmail.com>
2026-06-03 19:13:58 -0600

    fix: remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://y0naldez.github.io',
-	base: '/y0naldez',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
