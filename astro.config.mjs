306b142997080251a87e6a3490508f6da230c45d
y0naldez <yonaldez0@gmail.com>
2026-06-03 19:10:17 -0600

    fix: add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://y0naldez.github.io',
+	base: '/y0naldez',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
